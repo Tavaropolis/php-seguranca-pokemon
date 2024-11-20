3f0d4a54ed2fa4bfe37b3aa140162b8903675fb6
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-11-20 12:14:41 -0300

    feature: creating new user

--- newAccount.php
+++ newAccount.php
@@ -1,3 +1,6 @@
+<?php
+    require "pdoconn.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,24 +30,23 @@
         <form action="#" method="post">
             <div class="label-input">
                 <label for="user">Username</label>
-                <input type="text" name="user">
+                <input type="text" name="user" required>
             </div>
             <div class="label-input">
                 <label for="email">Email</label>
-                <input type="email" name="email">
-
+                <input type="email" name="email" required>
             </div>
             <div class="label-input">
                 <label for="password">Password</label>
                 <div class="password-container">
-                    <input type="password" name="password">
+                    <input type="password" name="password" required>
                     <img src="./assets/visibility.svg" alt="" class="visible-icon">
                 </div>
             </div>
             <div class="label-input">
                 <label for="password-confirm">Confirmar Password</label>
                 <div class="password-container">
-                    <input type="password" name="password-confirm">
+                    <input type="password" name="password-confirm" required>
                     <img src="./assets/visibility.svg" alt="" class="visible-icon">
                 </div>
             </div>
@@ -54,6 +56,101 @@
             </div>
         </form>
     </div>
+    <?php 
+            if(isset($_POST['user'], $_POST['email'], $_POST['password'], $_POST['password-confirm'])) {
+
+                $encrypt = encryptPassword($_POST['password']);
+
+                $salt = $encrypt[0];
+                $password = $encrypt[1];
+                $pepper = $encrypt[2];
+
+                $checkUser = checkAvaibleUser($_POST['user']);
+                if(count($checkUser)) {
+                    echo "<p>Usuário já cadastrado</p>";
+                    exit();
+                }
+
+                $checkEmail = checkAvaibleEmail($_POST['email']);
+                if(count($checkEmail)) {
+                    echo "<p>Email já cadastrado</p>";
+                    exit();
+                }
+
+                if(strlen($_POST['password']) <= 4) {
+                    echo "<p>Senha muito curta. Ela deve ter mais de 4 digítos</p>";
+                    exit(); 
+                }
+
+                if($_POST['password'] != $_POST['password-confirm']) {
+                    echo "<p>Senha e econfirmação de senha inválidos</p>";
+                    exit();
+                }
+                
+                $msg = insertNewUser($_POST['user'], $_POST['email'], $salt, $password, $pepper);
+                echo $msg;
+            } 
+        ?>
    </main>
 </body>
 </html>
+
+<?php
+    function encryptPassword(string $password): array {
+        $salt = bin2hex(random_bytes(16));
+        $pepper = bin2hex(random_bytes(16));
+        $password = $salt.$password.$pepper;
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        return [$salt, $password, $pepper];
+    }
+
+    function checkAvaibleUser(string $user): array {
+        global $conn;
+
+        try {
+            $checkUser = $conn->prepare("SELECT username FROM users WHERE username = :user LIMIT 1");
+            $checkUser->bindParam(':user', $user);
+            $checkUser->execute();
+            $checkUser = $checkUser->fetchAll(PDO::FETCH_ASSOC); 
+
+            return $checkUser ? $checkUser : [];
+        } catch(Exception $e) {
+            return $e;
+        }
+    }
+
+    function checkAvaibleEmail(string $email): array {
+        global $conn;
+
+        try {
+            $checkEmail = $conn->prepare("SELECT email FROM users WHERE email = :email LIMIT 1");
+            $checkEmail->bindParam(':email', $email);
+            $checkEmail->execute();
+            $checkEmail = $checkEmail->fetchAll(PDO::FETCH_ASSOC); 
+
+            return $checkEmail ? $checkEmail : [];
+        } catch(Exception $e) {
+            return $e;
+        }
+    }
+
+    function insertNewUser(string $user, string $email, string $salt, string $password, string $pepper): string {
+        global $conn;
+
+        try {
+            $insertUser =  $conn->prepare("INSERT INTO users(username, email, salt, userPassword, pepper, roleId) VALUES (:user, :email, :salt, :password, :pepper, 2)");
+            $insertUser->bindParam(':user', $user);
+            $insertUser->bindParam(':email', $email);
+            $insertUser->bindParam(':salt', $salt);
+            $insertUser->bindParam(':password', $password);
+            $insertUser->bindParam(':pepper', $pepper);
+
+            $insertUser->execute();
+
+            return "<p>Usuário cadastrado com sucesso</p>";
+        } catch(Exception $e) {
+            return "<p>Erro ao cadastrar usuário</p>";
+        }
+    }
+?>
